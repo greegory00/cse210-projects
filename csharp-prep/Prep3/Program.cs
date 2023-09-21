b2a8596ad6915fbc66f761561ed1bfa750c026f7
greegory00 <gregorymunsmi@gmail.com>
2023-09-21 23:07:58 +0200

    Prep 3, 4 and 5

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1, 100);
+
+        int guess = -1;
+        
+        while (guess != num)
+        {
+        
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            
+
+                if (num > guess)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else if (num < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+        }
+        
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,56 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = -1;
+        
+        
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            
+            string num = Console.ReadLine();
+            number = int.Parse(num);
+                 
+        
+            if (number != 0)
+            {
+            numbers.Add(number);
+            }
+
+
+
+
+        }
+
+        int sum = 0;
+        foreach (int n in numbers)
+        {
+        sum += n;
+        }
+    
+        float average = ((float)sum) / numbers.Count;
+
+        int max = numbers[0];
+
+        foreach (int nu in numbers)
+
+        {
+            if (nu > max)
+            {
+            max = nu;
+            }
+            
+        }
+        
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
